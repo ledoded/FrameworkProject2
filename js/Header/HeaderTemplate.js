d74cbe955431e85f44c38eac291e13d37ba12ba5
ledoded <133365946+ledoded@users.noreply.github.com>
2023-06-08 20:47:26 +0400

    framework

---
+++ js/Header/HeaderTemplate.js
@@ -0,0 +1,5 @@
+Template.prototype.headerTemplate = () => `
+    <button class="showComponent" data-component="graph2D">Calculator</button>
+    <button class="showComponent" data-component="graph3D">2D Graph</button>
+    <button class="showComponent" data-component="calculator">3D Graphs</button>
+    `;
